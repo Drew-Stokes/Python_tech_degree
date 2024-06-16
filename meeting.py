d0be92022cb4b298b182f9af51126fe696c81b59
Andrew Stokes <Drew.l.stokes.navy@gmail.com>
2024-06-15 23:13:45 -0600

    Update meeting.py
    Added wish list functionality

--- meeting.py
+++ meeting.py
@@ -4,3 +4,14 @@ attendees.extend(["James", "Gull"])
 optional_invitees = ["Ben J.", "Dave"]
 potential_attendees = attendees + optional_invitees
 print("There are", len(potential_attendees), "potential attendees currently")
+
+books = [
+    "Learning Python: Powerful Object-Oriented Programming - Mark Lutz",
+    "Automate the Boring Stuff with Python: Practical Programming for Total Beginners - Al Sweigart",
+    "Python for Data Analysis - Wes McKinney",
+    "Fluent Python: Clear, Concise, and Effective Programming - Luciano Ramalho",
+    "Python for Kids: A Playful Introduction To Programming - Jason R. Briggs",
+    "Hello Web App: Learn How to Build a Web App - Tracy Osborn",
+]
+
+print("Suggested gift: {}".format(books[0])
