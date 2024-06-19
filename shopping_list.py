7892750dd11c133ece2b83fda79bed5738cfcbb3
Andrew Stokes <Drew.l.stokes.navy@gmail.com>
2024-06-19 00:32:32 -0600

    Shopping list append modified
    Delete functionality and list display added

--- shopping_list.py
+++ shopping_list.py
@@ -1,16 +1,24 @@
-# Create a new empty list named shopping_list
 shopping_list = []
-# Create a a function named add_to_list that declares a parameter named item
-def add_to_list(item):
-    # Add the item to the list
-    shopping_list.append.item
+
 def show_help():
     print("What should we pick up at the store?")
     print("""
 Enter 'DONE' to stop adding items.
 Enter 'HELP' for this help.
+Enter 'SHOW' to see your current list.
 """)
+
     
+
+def add_to_list(item):
+    shopping_list.append(item)
+    print("Added! List has {} items.".format(len(shopping_list)))
+
+# Define a function named show_list that prints all the items in the list
+def show_list():
+    print("Here's your list:")
+    for item in shopping_list:
+        print(item)    
 show_help()
 while True:
     new_item = input("> ")
@@ -20,6 +28,12 @@ while True:
     elif new_item == 'HELP':
         show_help()
         continue
+    # Enable the SHOW command to show the list. Don't forget to update the HELP documentation.
+    #HINT. Make sure to run it.
+    elif new_item == 'SHOW':
+        show_list()
+        continue
 
-    # Call add_to_list with new_item as an argument
     add_to_list(new_item)
+
+show_list()
