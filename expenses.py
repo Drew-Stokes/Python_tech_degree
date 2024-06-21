3125e926a2e23f852c55a4c2ac92ab29178ea2fc
Andrew Stokes <Drew.l.stokes.navy@gmail.com>
2024-06-20 23:20:19 -0600

    updating expenses.py
    iterating through musical groups

--- expenses.py
+++ expenses.py
@@ -9,3 +9,13 @@ week_number = 1
 for week in travel_expenses:
     print("* week #{}".format(week_number, sum(week)))
     week_number += 1
+
+musical_groups = [
+    ["Ad Rock", "MCA", "Mike D."],
+    ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"],
+    ["Salt", "Peppa", "Spinderella"],
+    ["Rivers Cuomo", "Patrick Wilson", "Brian Bell", "Scott Shriner"],
+    ["Chuck D.", "Flavor Flav", "Professor Griff", "Khari Winn", "DJ Lord"],
+    ["Axl Rose", "Slash", "Duff McKagan", "Steven Adler"],
+    ["Run", "DMC", "Jam Master Jay"],
+]
